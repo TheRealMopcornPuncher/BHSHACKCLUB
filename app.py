089acfc6488b2482f6ec6911b2a49d2db2a5d465
Amaya <m0pcornpuncher@gmail.com>
2025-01-20 21:12:26 -0600

    Finish homepage, about page, add routes

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/resources")
+def resources():
+    return render_template("resources.html")
+
+@app.route("/events")
+def events():
+    return render_template("events.html")
