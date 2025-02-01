14751df5624f582d3fbb8a285f2a47bbf68b537e
Amaya <m0pcornpuncher@gmail.com>
2025-02-01 12:38:12 -0600

    Slight UI tweaks

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+@app.context_processor
+def inject_request():
+    return dict(request=request)
 
 @app.route("/")
 def hello_world():
@@ -17,3 +21,6 @@ def resources():
 @app.route("/events")
 def events():
     return render_template("events.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
